0227031fd74417ceeab823568e952b00f99a7a72
池フクロウ <mtaketani37@gmail.com>
2022-05-09 05:44:09 +0900

    検索機能の実装準備

--- search.php
+++ search.php
@@ -3,6 +3,15 @@
   include 'lib/daily.php';
   include 'lib/queryDaily.php';
 
+
+  //画面から取得した検索項目を受け取る
+  if ((!empty($_POST['tgtFromdate']) || !empty($_POST['tgtTodate'])) || (!empty($_GET['tgtcategory'])))
+  {
+
+
+  }
+
+
   //カレンダー画面から取得した「月」と「日」
   // $yearmonth = $_GET['yearmonth'];
   $yearmonth = '2022/04';
@@ -34,35 +43,37 @@
     <?php include 'header.php';?>
     <main>
     <!-- 検索要テーブル -->
-    <table class="search-table">
-      <tbody>
-        <tr>
-          <th>日付(From)</th>
-          <td><label><input type="date" name="tgtdate" size="30" value=""></label></td>
-          <th>日付(From)</th>
-          <td><label><input type="date" name="tgtdate" size="30" value=""></label></td>
-          <th>カテゴリー</th>
-          <td>
-            <div class="category cate">
-              <select name="tgtcategory">
-                <option value="" hidden>選ぶ</option>
-                <option value="食費">食費</option>
-                <option value="日用品">日用品</option>
-                <option value="Amazon">Amazon</option>
-                <option value="医療費">医療費</option>
-                <option value="公共料金">公共料金</option>
-                <option value="税金">税金</option>
-                <option value="外食">外食</option>
-                <option value="その他">その他</option>
-              </select>
-            </div>
-          </td>
-          <td>
-          <button class="button">検索</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <form action="search.php" method="post">
+      <table class="search-table">
+        <tbody>
+          <tr>
+            <th>日付(From)</th>
+            <td><label><input type="date" name="tgtFromdate" size="30" value=""></label></td>
+            <th>日付(From)</th>
+            <td><label><input type="date" name="tgtTodate" size="30" value=""></label></td>
+            <th>カテゴリー</th>
+            <td>
+              <div class="category cate">
+                <select name="tgtcategory">
+                  <option value="" hidden>選ぶ</option>
+                  <option value="食費">食費</option>
+                  <option value="日用品">日用品</option>
+                  <option value="Amazon">Amazon</option>
+                  <option value="医療費">医療費</option>
+                  <option value="公共料金">公共料金</option>
+                  <option value="税金">税金</option>
+                  <option value="外食">外食</option>
+                  <option value="その他">その他</option>
+                </select>
+              </div>
+            </td>
+            <td>
+            <button class="button">検索</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
     <table  class="result-table">
             <div class="headinfo">
               <div class="tgthead">2022年05月05日</div>
@@ -71,17 +82,24 @@
             </div>
             <tbody>
               <?php 
-                foreach($results as $result){
-                  echo "<tr>";
-                  echo "<th>出費</th>";
-                  echo "<td>".$result["tgtmoney"]."</td>";
-                  echo "<th>カテゴリー</th>";
-                  echo "<td>".$result["tgtcategory"]."</td>";
-                  echo "<th>品目</th>";
-                  echo "<td>".$result["tgtitem"]."</td>";
-                  echo "<th>カロリー</th>";
-                  echo "<td>".$result["tgtcalory"]."</td>";
-                  echo "</tr>";
+                if(!empty($results))
+                {
+                  foreach($results as $result){
+                    echo "<tr>";
+                    echo "<th>出費</th>";
+                    echo "<td>".$result["tgtmoney"]."</td>";
+                    echo "<th>カテゴリー</th>";
+                    echo "<td>".$result["tgtcategory"]."</td>";
+                    echo "<th>品目</th>";
+                    echo "<td>".$result["tgtitem"]."</td>";
+                    echo "<th>カロリー</th>";
+                    echo "<td>".$result["tgtcalory"]."</td>";
+                    echo "</tr>";
+                  }
+                }
+                else
+                {
+                    echo "データが存在しません";
                 }
               ?>
             </tbody>
